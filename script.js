9dd4f0a213360b53ca50d56a8210d8f82b6ae107
kaczmarekpawel90 <116645768+kaczmarekpawel90@users.noreply.github.com>
2022-11-22 12:55:56 +0000

    aall

---
+++ script.js
@@ -0,0 +1,7 @@
+function lyrics(){
+    for(i=99;i>=2;i--){
+        document.getElementById("main").innerHTML+="<br />"+i+" bottles of beer on the wall, "+i+" bottles of beer <br /> Take one down and pass it around, "+(i-1)+" bottles of beer on the wall<br />";
+    }
+    document.getElementById("main").innerHTML+="<br />1 bottle of beer on the wall, 1 bottle of beer<br />Take one down and pass it around, no more bottles of beer on the wall<br />";
+    document.getElementById("main").innerHTML+="<br />No more bottles of beer on the wall, no more bottles of beer<br /> Go to the store and buy some more, 99 bottles of beer on the wall"
+}
